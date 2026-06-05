14dfeae637fd923d6692554c7e01617ef8e6d93b
anuj <anuj@gmail.com>
2026-06-05 18:26:46 +0530

    file moved

---
+++ mano01.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#define size 5
+int arr[size];
+int il=0;
+int ub=4;
+void sort(int,int);
+void sort(int a, int b)
+{
+ for(int i=a; i<=ub; i++)
+{
+ cout<<arr[i]<<" ";
+}
+}
+
+int main()
+{
+  sort(lb, ub);
+  return 0;
+}
